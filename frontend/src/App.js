a7d2330a37a796d7ebcb379c8c74acec3d13d662
Arllette Fonseca <arllette.fonseca@gmail.com>
2021-06-08 10:46:55 -0700

    prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,26 @@ import Search from './components/ Search';
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('')
+  const [word, setWord] = useState('');
   const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err)=> {
-      console.log(err);
-    })
+    e.preventDefault();
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     setWord('');
-}
+  };
 
   return (
     <div className="App">
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
