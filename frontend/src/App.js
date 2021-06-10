1ed51312fe50ccec3b5e2e83041f42db40dd9c05
Arllette Fonseca <arllette.fonseca@gmail.com>
2021-06-10 13:33:29 -0700

    imagecard component complete along with delete functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
-  console.log('images array', images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -26,7 +25,9 @@ const App = () => {
       });
     setWord('');
   };
-
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
   return (
     <div className="App">
       <Header title="Images Gallery" />
@@ -35,7 +36,7 @@ const App = () => {
         <Row xs={1} md={2} lg={3}>
           {images.map((image, i) => (
             <Col key={i} className="pb-3">
-              <ImageCard image={image} />
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
             </Col>
           ))}
         </Row>
